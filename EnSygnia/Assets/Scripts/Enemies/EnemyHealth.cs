e0ebf4bce6aa01fe137a15e04a3cb7d437c6a907
MaxwellRunnels <maxwellrunnels@gmail.com>
2024-11-25 14:51:03 -0500

    Most Recent Push
    :)

---
+++ EnSygnia/Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int startingHealth = 3;
+    [SerializeField] private GameObject DeathVFXPrefab;
+    [SerializeField] private float knockBackThrust = 10f;
+
+    private int currentHealth;
+    private Knockback knockback;
+    private Flash flash;
+
+    private void Awake() {
+        knockback = GetComponent<Knockback>();
+        flash = GetComponent<Flash>();
+    }
+
+    private void Start() {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+        knockback.GetKnockedBack(PlayerController.Instance.transform, knockBackThrust);
+        StartCoroutine(flash.FlashRoutine());
+        StartCoroutine(CheckDetectDeathRoutine());
+    }
+
+    private IEnumerator CheckDetectDeathRoutine() {
+        yield return new WaitForSeconds(flash.GetRestoreMatTime());
+        DetectDeath();
+    }
+
+    private void DetectDeath() {
+        if (currentHealth <= 0) {
+            Instantiate(DeathVFXPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
